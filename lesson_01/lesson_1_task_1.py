f4f794f93df736ce33f815d2fbfa0439534fe92f
gaa771 <gaa.771@gmail.com>
2025-07-06 21:35:51 +0300

    Test1

---
+++ lesson_01/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Артем"
+print(my_name)
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def phone_num(num):
+    print(num, end="")
+
+
+phone_num(8)
+phone_num(8)
+phone_num(0)
+phone_num(0)
+phone_num(5)
+phone_num(5)
+phone_num(5)
+phone_num(3)
+phone_num(5)
+phone_num(3)
+phone_num(5)
